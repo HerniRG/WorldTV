9270b17db3e4d04f229d53c7e42620c32b8bbadc
Hernan Rodriguez <hernanrg85@gmail.com>
2026-07-28 09:05:43 +0200

    Initial Commit

---
+++ WorldTV/WorldTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTVApp.swift
+//  WorldTV
+//
+//  Created by Hernan Rodriguez on 28/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
